5f44f9085a5051b1aa899f6229e7d0ff10517ff5
Miran-Mirantee <killerm11805@hotmail.com>
2023-02-11 22:17:19 +0700

    Implement tail and fix head in linkedList class

--- linkedList.js
+++ linkedList.js
@@ -8,11 +8,22 @@ class LinkedList {
     const node = new Node(value);
     if (this.headNode === null) {
       this.headNode = node;
+    } else {
+      let next = this.headNode;
+      while (next.nextNode != null) {
+        next = next.nextNode;
+      }
+      next.nextNode = node;
+      this.tailNode = node;
     }
   }
 
   head() {
     console.log(this.headNode);
+  }
+
+  tail() {
+    console.log(this.tailNode);
   }
 }
 
@@ -27,5 +38,9 @@ const list = new LinkedList();
 list.append("what");
 list.append("what2");
 list.append("what3");
+list.append("what4");
+list.append("what5");
+list.append("what6");
 console.log(list);
 list.head();
+list.tail();
